db32380faf7eb81f5b3b8d7ebbc709f648f9ac4a
Hamza Waleed <hamzawaleed0102@gmail.com>
2019-07-18 11:11:27 +0500

    State Integrated

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
+  var questions = ["What's your name?", "What's your age"];
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Flutter Starter")),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Text in column"),
-            Text("New text element"),
+            Text(questions[questionIndex]),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 RaisedButton(
-                  child: Text("Row Button A"),
-                  onPressed: null,
+                  child: Text("Answer 1"),
+                  onPressed: answerQuestion,
                 ),
                 RaisedButton(
-                  child: Text("Row Button B"),
-                  onPressed: null,
+                  child: Text("Answer 2"),
+                  onPressed: answerQuestion,
+                ),
+                RaisedButton(
+                  child: Text("Answer 2"),
+                  onPressed: answerQuestion,
                 ),
               ],
             ),
-            RaisedButton(
-              child: Text("A Button "),
-              onPressed: null,
-            )
           ],
         ),
       ),
